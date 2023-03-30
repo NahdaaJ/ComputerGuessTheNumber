cac701ef0b90de7d987746cc8b84493fb534c718
Nahdaa <jnahdaa@gmail.com>
2023-03-30 16:34:23 +0100

    Functional game script created.

---
+++ cgtnScript.py
@@ -0,0 +1,30 @@
+# COMPUTER GUESS THE NUMBER
+# Author: Nahdaa Jawed
+# Date Started: 30/03/2023 15:38
+# Date Finished: -
+# Description: -
+
+import random
+
+def guessNum():
+    goLow = 100
+    goHigh = 1
+    userFeedback = ''
+
+    while userFeedback!= "Y":
+        if goLow != goHigh:
+            compGuess = random.randint(goHigh, goLow)
+        else:
+            compGuess = goLow
+
+        userFeedback = input(f'\nIs {compGuess} your number?\nHigher (H), Lower (L), Yes (Y) ')
+        if userFeedback == "H":
+            goHigh = compGuess+1
+
+        elif userFeedback == "L":
+            goLow = compGuess - 1
+
+    print(f'\nThe computer has guessed the number {compGuess} correctly!')
+
+
+guessNum()
