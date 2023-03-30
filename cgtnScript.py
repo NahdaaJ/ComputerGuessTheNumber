6a0747857bb2a4fa42d7c4c1ae744a91f0dcc05e
Nahdaa <jnahdaa@gmail.com>
2023-03-30 16:52:47 +0100

    Added the option to play again.

--- cgtnScript.py
+++ cgtnScript.py
@@ -11,24 +11,35 @@ def guessNum():
     goHigh = 1
     userFeedback = ''
     countGuess = 0
+    playAgain = 0
 
-    while userFeedback!= "Y":
-        if goLow != goHigh:
-            compGuess = random.randint(goHigh, goLow)
-        else:
-            compGuess = goLow
+    while playAgain != "N":
 
-        userFeedback = input(f'\nIs {compGuess} your number?\nHigher (H), Lower (L), Yes (Y) ')
-        if userFeedback == "H":
-            goHigh = compGuess+1
+        while userFeedback!= "Y":
+            if goLow != goHigh:
+                compGuess = random.randint(goHigh, goLow)
+            else:
+                compGuess = goLow
 
-        elif userFeedback == "L":
-            goLow = compGuess - 1
+            userFeedback = input(f'\nIs {compGuess} your number?\nGo Higher (H), Go Lower (L), Yes (Y) ')
+            if userFeedback == "H":
+                goHigh = compGuess+1
 
-        countGuess = countGuess + 1
+            elif userFeedback == "L":
+                goLow = compGuess - 1
 
-    print(f'\nThe computer has guessed the number {compGuess} correctly!'
-          f'\nIt took the computer {countGuess} guesses!')
+            countGuess = countGuess + 1
 
+        print(f'\nThe computer has guessed the number {compGuess} correctly!'
+            f'\nIt took the computer {countGuess} guesses!')
+
+        playAgain = input("\nWould you like to play again? (Y/N)\n")
+        if playAgain == "Y":
+            goLow = 100
+            goHigh = 1
+            userFeedback = ''
+            countGuess = 0
+
+    print("\nThanks for playing!")
 
 guessNum()
